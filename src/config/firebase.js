9fdeead9a1b98498371e07562f9265beef2d220c
Lijerbul LJOBOY <jonathanyombo@gmail.com>
2020-04-29 21:27:30 +0200

    [UPDATE] change firebase config

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,13 +5,13 @@ import 'firebase/firestore';
 import 'firebase/messaging';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCOl0qzkhCDmyWeeIT8AZe4xyJJlIfU5gE",
-  authDomain: "myaurapp.firebaseapp.com",
-  databaseURL: "https://myaurapp.firebaseio.com",
-  projectId: "myaurapp",
-  storageBucket: "myaurapp.appspot.com",
-  messagingSenderId: "419511997060",
-  appId: "1:419511997060:web:ce7aa0d308f6b484f868d1"
+  apiKey: "AIzaSyCGjN2xc1OU1RTmGeuYX_xdrKrchnPLTFU",
+  authDomain: "gdg-lubumbashi-site.firebaseapp.com",
+  databaseURL: "https://gdg-lubumbashi-site.firebaseio.com",
+  projectId: "gdg-lubumbashi-site",
+  storageBucket: "gdg-lubumbashi-site.appspot.com",
+  messagingSenderId: "879926740587",
+  appId: "1:879926740587:web:78e5f34ada633c47bd532e"
 };
 
 firebase.initializeApp(firebaseConfig);
